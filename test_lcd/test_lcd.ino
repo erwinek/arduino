f66daeba7a7a5547ebc14342029540c8011b0526
Leszek Kula <erwinek@o2.pl>
2016-03-11 18:01:30 +0100

    Update test_lcd.ino

--- test_lcd/test_lcd.ino
+++ test_lcd/test_lcd.ino
@@ -6,15 +6,13 @@ const int lcd_mosi = 9;
 const int lcd_cs = 13;
 const int lcd_reset = 11;
 
-//RS — (CS) Начало/окончание передачи данных (1 — начало, 0 — окончание)
-//RW — (SID) Шина данных
-//E — (SCLK) Строб
-
 U8GLIB_ST7920_128X64_1X u8g(lcd_cs );
 
 void setup() {  
   u8g.setFont(u8g_font_unifont);
   u8g.setColorIndex(1); // Instructs the display to draw with a pixel on. 
+  
+  Serial.begin(9600);
 }
 
 void loop() {  
@@ -22,6 +20,9 @@ void loop() {
   do {  
     draw();
   } while( u8g.nextPage() );
+  
+  Serial.print("HelloWorld");
+  
   delay(1000);   
 }
   
@@ -30,35 +31,4 @@ void draw(){
     
 }
 
-/*
-void setup() {
-  // put your setup code here, to run once:
-  u8g.setFont(u8g_font_unifont);
-  u8g.setColorIndex(1); // Instructs the display to draw with a pixel on. 
-    Serial.begin(9600);
 
-  //  u8g.begin();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  //draw();
-
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.print("Hello");
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  Serial.print("World");
-  delay(1000);              // wait for a second
-}
-
-void draw(void) {
-  // graphic commands to redraw the complete screen should be placed here  
-  //u8g.setFont(u8g_font_unifont);
-  //u8g.drawStr(1, 1, "Hello World!");
-
-  
-
-  
-}
-*/
