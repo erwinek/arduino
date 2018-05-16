d546be9aaff29cf6a2bcd04bac1c9b543e76e7b3
Leszek Kula <erwinek@o2.pl>
2018-05-16 23:00:18 +0200

    Update prog.bat

--- ProgFlashWarrior/prog.bat
+++ ProgFlashWarrior/prog.bat
@@ -1,1 +1,1 @@
-esptool.exe -vv -cd nodemcu -cb 921600 -cp COM5 -ca 0x00000 -cf C:\Users\erwinek\AppData\Local\Temp\arduino_build_30108/3xLed.ino.bin 
+esptool.exe -vv -cd nodemcu -cb 921600 -cp COM5 -ca 0x00000 -cf 3xLed.ino.bin 
