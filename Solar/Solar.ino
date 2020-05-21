c8096d0f72ba444f54e6b2ab48c16649e77ab9ab
erwinek <erwinek@o2.pl>
2020-05-21 10:48:48 +0200

    wifi

--- Solar/Solar.ino
+++ Solar/Solar.ino
@@ -1,10 +1,19 @@
 #include <DallasTemperature.h>               //dodaj biblitekę LiquidCrystal.h
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 
 #define HISTEREZA 5
 #define TEMP_MAX 70
 #define ONE_WIRE_BUS D13 
 #define SOLAR_PUMP_PIN D15 
 
+#ifndef STASSID
+#define STASSID "ASUS"
+#define STAPSK  "HasloWifi123"
+#endif
+
+const char * ssid = STASSID; // your network SSID (name)
+const char * pass = STAPSK;  // your network password
 
 int one_wire = ONE_WIRE_BUS;                           //Transmisja 1-Wire na pinie 10
 bool SolarPumpOn = false;
@@ -12,14 +21,56 @@ OneWire oneWire(one_wire);                   //wywołujemy transmisję 1-Wire na
 DallasTemperature sensors(&oneWire);         //informujemy Arduino, ze przy pomocy 1-Wire
                                              //chcemy komunikowac sie z czujnikiem
                                              
- 
-
 void setup(void)
 {
   Serial.begin(9600);
   sensors.begin();                           //rozpocznij odczyt z czujnika
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   pinMode(SOLAR_PUMP_PIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+    ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
  
@@ -67,6 +118,7 @@ void loop(void)
     Serial.println("PUMP OFF");
   }
   
-  delay(1000);                                //odczekaj 500ms
+  delay(1);                                //odczekaj 500ms
+  ArduinoOTA.handle();
  
 }
